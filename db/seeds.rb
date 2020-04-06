783a87e87dec50216b0a5cd87e91cad50a590aec
GeorgeCoops <coopsgeorge@gmail.com>
2020-04-06 15:26:52 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.destroy_all
+
+Coupon.create(coupon_code: "HOT", store: "Nandos")
+Coupon.create(coupon_code: "DECK", store: "B&Q")
+Coupon.create(coupon_code: "Horse", store: "Tescos")
